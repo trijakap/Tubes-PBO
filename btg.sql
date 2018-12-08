c6855644f374ddf94ce2f84e36311e86601f48a9
Tri Jaka Pamungkas <33062165+trijakap@users.noreply.github.com>
2018-12-08 12:57:14 +0700

    Add files via upload

--- btg.sql
+++ btg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 07, 2018 at 01:43 PM
+-- Generation Time: Dec 08, 2018 at 06:56 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -57,6 +57,15 @@ CREATE TABLE `angkot` (
   `kode` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `angkot`
+--
+
+INSERT INTO `angkot` (`no_pol`, `tarif`, `kapasitas`, `kode`) VALUES
+('D1324ZDC', 5000, 10, 1),
+('D1425DCX', 7500, 10, 2),
+('D1422DFD', 10000, 10, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -69,6 +78,15 @@ CREATE TABLE `bus` (
   `kapasitas` int(11) DEFAULT NULL,
   `nama` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bus`
+--
+
+INSERT INTO `bus` (`no_pol`, `tarif`, `kapasitas`, `nama`) VALUES
+('D1123FFD', 3000, 40, 'Bus A'),
+('D3241FRE', 5000, 40, 'Bus B'),
+('D6582KJL', 7000, 40, 'Bus C');
 
 -- --------------------------------------------------------
 
@@ -101,7 +119,7 @@ CREATE TABLE `non-wisata` (
   `id` varchar(6) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `alamat` varchar(70) DEFAULT NULL,
-  `deskripsi` varchar(150) DEFAULT NULL
+  `deskripsi` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -109,14 +127,12 @@ CREATE TABLE `non-wisata` (
 --
 
 INSERT INTO `non-wisata` (`id`, `nama`, `alamat`, `deskripsi`) VALUES
-('001', 'Non Wisata BK 1', 'Bandung Kota', 'mantap'),
-('002', 'Non Wisata BK 2', 'Bandung Kota', 'mantap'),
-('003', 'Non Wisata BB 1', 'Bandung Barat', 'mantap'),
-('004', 'Non Wisata BB 2', 'Bandung Barat', 'mantap'),
-('005', 'Non Wisata BU 1', 'Bandung Utara', 'mantap'),
-('006', 'Non Wisata BU 2', 'Bandung Utara', 'mantap'),
-('007', 'Non Wisata BS 1', 'Bandung Selatan', 'mantap'),
-('008', 'Non Wisata BS 2', 'Bandung Selatan', 'mantap');
+('001', 'Paskal Food Market', 'Bandung Kota', 'Food Market yang terletak di pasir kaliki'),
+('002', 'Yoghurt Cisangkuy', 'Bandung Kota', 'Menjual yoghurt'),
+('004', 'Pinisi Resto', 'Bandung Selatan', 'Alamat : Patengan, Rancabali, Bandung, Jawa Barat 40973'),
+('005', 'Saung Pengkolan', 'Bandung Utara', 'Alamat : KM 12.3 No 35, Jl. Raya Lembang, Gudangkahuripan, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391'),
+('006', 'Floating Market', 'Bandung Utara', 'Alamat : Jl. Grand Hotel No.33E, Jalur Kampung Leuit No.A1, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391'),
+('008', 'Waroeng Kuliner', 'Bandung Barat', 'Alamat : Kota Baru Parahyangan, Jalan Pitaniaga No. 7, Cipeundeuy, Padalarang, Cipeundeuy, Padalarang, Kabupaten Bandung Barat, Jawa Barat 40553');
 
 -- --------------------------------------------------------
 
@@ -128,7 +144,7 @@ CREATE TABLE `nonwisata` (
   `id` varchar(20) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `alamat` varchar(70) NOT NULL,
-  `deskripsi` varchar(150) NOT NULL
+  `deskripsi` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -142,6 +158,30 @@ CREATE TABLE `rute` (
   `lokasi` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `rute`
+--
+
+INSERT INTO `rute` (`noPolisi`, `lokasi`) VALUES
+('D1324ZDC', 'Bandung Kota'),
+('D1324ZDC', 'Bandung Barat'),
+('D1425DCX', 'Bandung Kota'),
+('D1425DCX', 'Bandung Barat'),
+('D1425DCX', 'Bandung Utara'),
+('D1422DFD', 'Bandung Kota'),
+('D1422DFD', 'Bandung Barat'),
+('D1422DFD', 'Bandung Utara'),
+('D1422DFD', 'Bandung Selatan'),
+('D1123FFD', 'Bandung Kota'),
+('D1123FFD', 'Bandung Utara'),
+('D3241FRE', 'Bandung Kota'),
+('D3241FRE', 'Bandung Utara'),
+('D3241FRE', 'Bandung Selatan'),
+('D6582KJL', 'Bandung Kota'),
+('D6582KJL', 'Bandung Utara'),
+('D6582KJL', 'Bandung Selatan'),
+('D6582KJL', 'Bandung Barat');
+
 -- --------------------------------------------------------
 
 --
@@ -152,7 +192,7 @@ CREATE TABLE `wisata` (
   `id` varchar(6) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `alamat` varchar(70) DEFAULT NULL,
-  `deskripsi` varchar(150) DEFAULT NULL,
+  `deskripsi` varchar(500) DEFAULT NULL,
   `harga_tiket` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -161,15 +201,13 @@ CREATE TABLE `wisata` (
 --
 
 INSERT INTO `wisata` (`id`, `nama`, `alamat`, `deskripsi`, `harga_tiket`) VALUES
-('001', 'Wisata BK 1', 'Bandung Kota', 'mantap', 30000),
-('002', 'Wisata BK 2', 'Bandung Kota', 'mantap', 20000),
-('003', 'Wisata BB 1', 'Bandung Barat', 'mantap', 20000),
-('004', 'Wisata BB 2', 'Bandung Barat', 'mantap', 40000),
-('005', 'Wisata BU 1', 'Bandung Utara', 'mantap', 50000),
-('006', 'Wisata BU 2', 'Bandung Utara', 'mantap', 10000),
-('007', 'Wisata BS 1', 'Bandung Selatan', 'mantap', 10000),
-('008', 'Wisata BS 2', 'Bandung Selatan', 'mantap', 20000),
-('009', 'a', 'Bandung Kota', 'a', 130000);
+('001', 'Babakan Siliwangi', 'Bandung Kota', 'Babakan Siliwangi atau yang dikenal disingkat dengan Baksil adalah salah satu kawasan hutan kota di Bandung, tepatnya berada di Kelurahan Babakan Siliwangi, Kecamatan Coblong, Kota Bandung. Lokasi Babakan Siliwangi yang tidak jauh dari pusat kota (berada di utara pusat kota Bandung) membuat Babakan Siliwangi menjadi Ruang Terbuka Hijau (RTH) yang paling terjangkau oleh masyarakat kota Bandung. ', 0),
+('002', 'Trans Studio Bandung', 'Bandung Kota', 'Taman hiburan keluarga indoor dengan wahana seru, bioskop, & area bermain untuk anak usia dini.', 180000),
+('003', 'Kawah Putih Ciwidey', 'Bandung Selatan', 'Kawah Putih adalah sebuah tempat wisata di Jawa Barat yang terletak di kawasan Ciwidey. Kawah putih merupakan sebuah danau yang terbentuk dari letusan Gunung Patuha. Tanah yang bercampur belerang di sekitar kawah ini berwarna putih, lalu warna air yang berada di kawah ini berwarna putih kehijauan, yang unik dari kawah ini adalah airnya kadang berubah warna. Kawah ini berada pada ketinggian +2090 m dpl dibawah puncak/titik tertinggi Gunung Patuha. ', 18000),
+('004', 'Situ Cileunca', 'Bandung Selatan', 'Situ Cileunca adalah sebuah danau buatan dengan luas mencapai 1.400 hektar dengan latar belakang perbukitan dan pegunungan yang indah. Selain difungsikan sebagai objek wisata, Situ Cileunca juga berfungsi sebagai pembangkit listrik tenaga air (PLTA). Air yang berasal dari danau tersebut dialirkan melalui Sungai Palayangan. Sungai ini pula yang sering dijadikan sebagai arena ber-arung jeram.', 2500),
+('005', 'Tangkuban Parahu', 'Bandung Utara', 'Tangkuban Parahu atau Gunung Tangkuban Perahu adalah salah satu gunung yang terletak di Provinsi Jawa Barat, Indonesia. Sekitar 20 km ke arah utara Kota Bandung, dengan rimbun pohon pinus dan hamparan kebun teh di sekitarnya, Gunung Tangkuban Perahu mempunyai ketinggian setinggi 2.084 meter. Bentuk gunung ini adalah Stratovulcano dengan pusat erupsi yang berpindah dari timur ke barat', 20000),
+('007', 'Curug Malela', 'Bandung Barat', 'Curug Malela atau dalam bahasa Indonesia berarti Air Terjun Malela memiliki sumber air terjun yang  berasal dari hulu sungai bagian Utara Gunung Kendeng, yaitu sebuah gunung berapi yang terletak di sebelah barat Ciwidey yang telah mati dan kemudian mengalir melintasi sungai cidadap ? Gunung Halu.', 10000),
+('008', 'Dago Dream Park', 'Bandung Barat', 'Dago Dream Park adalah resort di Bandung yang dibuat dengan mengusung konsep sebuah resort paduan etnik Jawa ? Sunda & Bali yang sangat eksotik. Lingkungan Tempat wisata di Bandung Utara ini didesain dengan tata ruang bernuansa kental pulau Dewata dengan arsitektur bangunan kombinasi Jawa ? Sunda yang unik.', 20000);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
